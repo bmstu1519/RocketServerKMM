507f986dc48bf83a3df28a0510b7b1af2eb93686
egor.mikhalev <mamo4ka21196@gmail.com>
2024-10-23 18:29:08 +0300

    added snackbar and dark theme support

--- composeApp/src/commonMain/kotlin/org/rocketserverkmm/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/rocketserverkmm/project/App.kt
@@ -1,7 +1,20 @@
 package org.rocketserverkmm.project
 
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.navigator.Navigator
 import coil3.ImageLoader
 import coil3.PlatformContext
@@ -11,19 +24,54 @@ import coil3.memory.MemoryCache
 import coil3.request.CachePolicy
 import coil3.request.crossfade
 import coil3.util.DebugLogger
+import com.apollographql.apollo.api.ApolloResponse
 import okio.FileSystem
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.rocketserverkmm.project.theme.RocketReserverKMMTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
+
+    RocketReserverKMMTheme {
+
         setSingletonImageLoaderFactory { context ->
             getAsyncImageLoader(context)
         }
-        Navigator(
-            LaunchListScreen()
+        val snackbarHostState = remember { SnackbarHostState() }
+        val tripBookedFlow = remember {
+            ProvideApolloClientSingleton.apolloClient.subscription(TripsBookedSubscription())
+                .toFlow()
+        }
+        val tripBookedResponse: ApolloResponse<TripsBookedSubscription.Data>? by tripBookedFlow.collectAsState(
+            initial = null
         )
+        LaunchedEffect(tripBookedResponse) {
+            if (tripBookedResponse == null) return@LaunchedEffect
+            val message = when (tripBookedResponse!!.data?.tripsBooked) {
+                null -> "Subscription error"
+                -1 -> "Trip cancelled"
+                else -> "Trip booked! 🚀"
+            }
+            snackbarHostState.showSnackbar(
+                message = message,
+                duration = SnackbarDuration.Short
+            )
+        }
+
+        Scaffold(
+            topBar = {
+                TopAppBar(title = { Text("RocketServerKMM") },)
+            },
+            snackbarHost = { SnackbarHost(snackbarHostState) },
+            ) { paddingValues ->
+            Box(Modifier.padding(paddingValues)) {
+                Navigator(
+                    LaunchListScreen()
+                )
+            }
+        }
     }
 }
 
