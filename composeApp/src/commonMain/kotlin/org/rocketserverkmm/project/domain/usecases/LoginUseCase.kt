b7a98d3448c6c26bc009f550d285d2cd048802c0
egor.mikhalev <mamo4ka21196@gmail.com>
2024-10-31 15:26:19 +0300

    added usecase

--- composeApp/src/commonMain/kotlin/org/rocketserverkmm/project/domain/usecases/LoginUseCase.kt
+++
@@ -1,8 +0,0 @@
-package org.rocketserverkmm.project.domain.usecases
-
-import org.rocketserverkmm.project.domain.models.login.LoginResult
-import org.rocketserverkmm.project.domain.repositories.LaunchRepository
-
-class LoginUseCase(private val repository: LaunchRepository) {
-    suspend fun invoke(email: String) : LoginResult = repository.login(email)
-}
