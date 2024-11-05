a433d54b9512b10670a179d3b97154e6b9f58eb0
egor.mikhalev <mamo4ka21196@gmail.com>
2024-11-05 13:55:24 +0300

    finished GetLaunchDetailsUseCase

--- composeApp/src/commonMain/kotlin/org/rocketserverkmm/project/domain/usecases/GetLaunchDetailsUseCase.kt
+++ composeApp/src/commonMain/kotlin/org/rocketserverkmm/project/domain/usecases/GetLaunchDetailsUseCase.kt
@@ -1,9 +1,19 @@
 package org.rocketserverkmm.project.domain.usecases
 
+import org.rocketserverkmm.project.KEY_TOKEN
 import org.rocketserverkmm.project.domain.models.launchDetails.LaunchDetailsResult
+import org.rocketserverkmm.project.domain.repositories.KeyVaultRepository
 import org.rocketserverkmm.project.domain.repositories.LaunchRepository
 
-class GetLaunchDetailsUseCase(private val repository: LaunchRepository){
-    suspend fun getLaunchDetails(launchId: String) : Result<LaunchDetailsResult> = repository.getLaunchDetails(launchId)
-    suspend fun tripMutation(launchId: String, isBooked: Boolean) : Result<Any> = repository.getTripMutation(launchId, isBooked)
+class GetLaunchDetailsUseCase(
+    private val repository: LaunchRepository,
+    private val keyVaultRepository: KeyVaultRepository
+) {
+    suspend fun getLaunchDetails(launchId: String): Result<LaunchDetailsResult> =
+        repository.getLaunchDetails(launchId)
+
+    fun checkToken(key: String = KEY_TOKEN): Boolean = keyVaultRepository.getToken(key) !== null
+
+    suspend fun tripMutation(launchId: String, isBooked: Boolean): Result<Any> =
+        repository.getTripMutation(launchId, isBooked)
 }
