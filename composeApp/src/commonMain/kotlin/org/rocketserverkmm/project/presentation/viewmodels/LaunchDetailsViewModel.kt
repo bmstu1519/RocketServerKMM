ce36a108e3c97b334cf81a0a1650add2950bf6a5
egor.mikhalev <mamo4ka21196@gmail.com>
2024-11-05 13:55:44 +0300

    finished LaunchDetailsViewModel

--- composeApp/src/commonMain/kotlin/org/rocketserverkmm/project/presentation/viewmodels/LaunchDetailsViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/rocketserverkmm/project/presentation/viewmodels/LaunchDetailsViewModel.kt
@@ -8,9 +8,6 @@ import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import org.rocketserverkmm.project.domain.models.launchDetails.MissionDTO
-import org.rocketserverkmm.project.domain.models.launchDetails.RocketDTO
-import org.rocketserverkmm.project.domain.models.launchDetails._LaunchDetailsState
 import org.rocketserverkmm.project.domain.usecases.GetLaunchDetailsUseCase
 import org.rocketserverkmm.project.presentation.states.ButtonState
 import org.rocketserverkmm.project.presentation.states.LaunchDetailsAction
@@ -28,6 +25,14 @@ class LaunchDetailsViewModel(
 
     private var _launchId: String = ""
 
+    init {
+        updateState(
+            launchDetailsState = LaunchDetailsState(
+                isLoading = true
+            )
+        )
+    }
+
     fun actionToDestination(action: LaunchDetailsAction) {
         when (action) {
             is LaunchDetailsAction.Load -> load(action.launchId)
@@ -42,16 +47,19 @@ class LaunchDetailsViewModel(
             result
                 .onSuccess { success ->
                     handleResult(
-                        _LaunchDetailsState(
+                        LaunchDetailsState(
+                            isLoading = false,
                             isBooked = success.isBooked,
                             mission = success.mission,
                             rocket = success.rocket,
+                            site = success.site
                         )
                     )
                 }
                 .onFailure { failed ->
                     handleResult(
-                        _LaunchDetailsState(
+                        LaunchDetailsState(
+                            isLoading = false,
                             errorMessage = failed.message
                         )
                     )
@@ -60,55 +68,76 @@ class LaunchDetailsViewModel(
     }
 
     private fun clickButton() {
-        viewModelScope.launch {
-            val isBooked = state.value.isBooked
-            isBooked?.let {
-                val result = getLaunchDetailsUseCase.tripMutation(_launchId, isBooked)
-                result
-                    .onSuccess {
-                        handleResult(
-                            _LaunchDetailsState(
-                                isBooked = !isBooked
+        val checkToken = getLaunchDetailsUseCase.checkToken()
+
+        if (checkToken) {
+            viewModelScope.launch {
+                val isBooked = state.value.isBooked
+                isBooked?.let {
+                    val result = getLaunchDetailsUseCase.tripMutation(_launchId, isBooked)
+                    result
+                        .onSuccess {
+                            handleResult(
+                                LaunchDetailsState(
+                                    isBooked = !isBooked
+                                )
                             )
-                        )
-                    }
-                    .onFailure { exception ->
-                        println("LaunchDetails: Failed to book/cancel trip ${exception.message}")
-                        handleResult(
-                            _LaunchDetailsState(
-                                bookedState = ButtonState.Error,
-                                errorMessage = exception.message
+                        }
+                        .onFailure { exception ->
+                            println("LaunchDetails: Failed to book/cancel trip ${exception.message}")
+                            handleResult(
+                                LaunchDetailsState(
+                                    bookedState = ButtonState.Error,
+                                    errorMessage = exception.message
+                                )
                             )
-                        )
-                    }
+                            ButtonState.Error.handleStateChange(2000L) { resetState() }
+                        }
+                }
             }
+        } else {
+            goToLogin()
         }
     }
 
-    private suspend fun handleResult(result: _LaunchDetailsState) {
+    private fun handleResult(result: LaunchDetailsState) {
         updateState(
-            bookedState = result.bookedState,
-            isBooked = result.isBooked,
-            mission = result.mission,
-            rocket = result.rocket,
-            errorMessage = result.errorMessage,
+            launchDetailsState = LaunchDetailsState(
+                isLoading = result.isLoading,
+                bookedState = result.bookedState,
+                isBooked = result.isBooked,
+                mission = result.mission,
+                rocket = result.rocket,
+                site = result.site,
+                errorMessage = result.errorMessage,
+            )
+        )
+    }
+
+    private fun goToLogin() {
+        viewModelScope.launch {
+            _destination.emit(LaunchDetailsDestination.GoToLogin)
+        }
+    }
+
+    private fun resetState(state: LaunchDetailsState? = null) {
+        updateState(
+            launchDetailsState = state
         )
     }
 
     private fun updateState(
-        bookedState: ButtonState? = null,
-        isBooked: Boolean? = null,
-        mission: MissionDTO? = null,
-        rocket: RocketDTO? = null,
-        errorMessage: String? = null,
+        launchDetailsState: LaunchDetailsState? = null,
     ) {
         _state.update { current ->
             current.copy(
-                bookedState = bookedState ?: current.bookedState,
-                isBooked = isBooked ?: current.isBooked,
-                mission = mission ?: current.mission,
-                rocket = rocket ?: current.rocket,
-                errorMessage = errorMessage ?: current.errorMessage,
+                isLoading = launchDetailsState?.isLoading ?: current.isLoading,
+                bookedState = launchDetailsState?.bookedState,
+                isBooked = launchDetailsState?.isBooked ?: current.isBooked,
+                mission = launchDetailsState?.mission ?: current.mission,
+                rocket = launchDetailsState?.rocket ?: current.rocket,
+                site = launchDetailsState?.site ?: current.site,
+                errorMessage = launchDetailsState?.errorMessage ?: current.errorMessage,
             )
         }
     }
