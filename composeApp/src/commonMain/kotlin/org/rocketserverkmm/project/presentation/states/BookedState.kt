873ee20857b9fbb419332a1b231450c3d287a0e7
egor.mikhalev <mamo4ka21196@gmail.com>
2024-11-10 20:04:48 +0300

    useless state

--- composeApp/src/commonMain/kotlin/org/rocketserverkmm/project/presentation/states/BookedState.kt
+++
@@ -1,9 +0,0 @@
-package org.rocketserverkmm.project.presentation.states
-
-import org.rocketserverkmm.project.domain.models.launchDetails.LaunchDetailsResult
-
-sealed class BookedState {
-    data object Loading : BookedState()
-    data class Error(val message: String) : BookedState()
-    data class Success(val data: LaunchDetailsResult) : BookedState()
-}
