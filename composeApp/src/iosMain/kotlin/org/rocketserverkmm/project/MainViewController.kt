fbe3f7ec4df23588f73e219836f8d9008440c36d
egor.mikhalev <mamo4ka21196@gmail.com>
2024-11-29 15:16:24 +0300

    connected coin to android and ios

--- composeApp/src/iosMain/kotlin/org/rocketserverkmm/project/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/rocketserverkmm/project/MainViewController.kt
@@ -1,5 +1,12 @@
 package org.rocketserverkmm.project
 
 import androidx.compose.ui.window.ComposeUIViewController
+import org.rocketserverkmm.project.dependencies.di.initKoin
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController(
+    configure = {
+        initKoin()
+    }
+) {
+    App()
+}
