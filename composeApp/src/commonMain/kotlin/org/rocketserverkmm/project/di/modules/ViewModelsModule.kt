4a43463acf25a4bf8305e4c7e03c6c0456b25803
egor.mikhalev <mamo4ka21196@gmail.com>
2024-12-24 17:21:30 +0300

    added di support for SettingsScreen

--- composeApp/src/commonMain/kotlin/org/rocketserverkmm/project/di/modules/ViewModelsModule.kt
+++ composeApp/src/commonMain/kotlin/org/rocketserverkmm/project/di/modules/ViewModelsModule.kt
@@ -1,29 +1,33 @@
 package org.rocketserverkmm.project.di.modules
 
 import org.koin.core.module.dsl.singleOf
-import org.koin.core.module.dsl.viewModel
 import org.koin.core.module.dsl.viewModelOf
-import org.koin.core.qualifier.named
 import org.koin.dsl.bind
 import org.koin.dsl.module
 import org.rocketserverkmm.project.domain.repositories.KeyVaultRepository
 import org.rocketserverkmm.project.domain.repositories.LaunchRepository
+import org.rocketserverkmm.project.domain.repositories.SettingsRepository
 import org.rocketserverkmm.project.domain.usecases.GetLaunchDetailsUseCase
 import org.rocketserverkmm.project.domain.usecases.GetLaunchesUseCase
 import org.rocketserverkmm.project.domain.usecases.GetLoginUseCase
+import org.rocketserverkmm.project.domain.usecases.GetSettingsUseCase
 import org.rocketserverkmm.project.presentation.viewmodels.LaunchDetailsViewModel
 import org.rocketserverkmm.project.presentation.viewmodels.LaunchListViewModel
 import org.rocketserverkmm.project.presentation.viewmodels.LoginViewModel
+import org.rocketserverkmm.project.presentation.viewmodels.SettingsViewModel
 import org.rocketserverkmm.project.repositories.KeyVaultRepositoryImpl
 import org.rocketserverkmm.project.repositories.LaunchRepositoryImpl
+import org.rocketserverkmm.project.repositories.SettingsRepositoryImpl
 
 val viewModelsModule = module {
     singleOf(::LaunchRepositoryImpl).bind<LaunchRepository>()
     singleOf(::KeyVaultRepositoryImpl).bind<KeyVaultRepository>()
+    singleOf(::SettingsRepositoryImpl).bind<SettingsRepository>()
 
     viewModelOf(::LaunchListViewModel)
     viewModelOf(::LoginViewModel)
     viewModelOf(::LaunchDetailsViewModel)
+    viewModelOf(::SettingsViewModel)
 
     factory {
         val launchRepository: LaunchRepository = get()
@@ -40,6 +44,13 @@ val viewModelsModule = module {
         val launchRepository: LaunchRepository = get()
         val keyVaultRepository: KeyVaultRepository = get()
         GetLoginUseCase(launchRepository, keyVaultRepository)
+    }
+
+    factory {
+        val launchRepository: LaunchRepository = get()
+        val keyVaultRepository: KeyVaultRepository = get()
+        val settingsRepository: SettingsRepository = get()
+        GetSettingsUseCase(launchRepository, keyVaultRepository, settingsRepository)
     }
 }
 
