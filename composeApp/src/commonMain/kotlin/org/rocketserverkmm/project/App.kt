cba82432f8088496bd84df881ca4b62e11836d5b
egor.mikhalev <mamo4ka21196@gmail.com>
2024-12-24 14:51:09 +0300

    implement tab bar to App.kt

--- composeApp/src/commonMain/kotlin/org/rocketserverkmm/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/rocketserverkmm/project/App.kt
@@ -5,10 +5,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -17,7 +21,10 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -29,13 +36,16 @@ import org.rocketserverkmm.project.di.LocalScopeManager
 import org.rocketserverkmm.project.di.ScopeManager
 import org.rocketserverkmm.project.platform.RocketReserverKMMTheme
 import org.rocketserverkmm.project.presentation.screens.LaunchListScreen
+import org.rocketserverkmm.project.presentation.screens.SettingsScreen
 import org.rocketserverkmm.project.settings.local.AsyncImageLoaderSingleton
+import org.rocketserverkmm.project.settings.tabSetting.TabItem
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 @Preview
 fun App() {
     val scopeManager = remember { ScopeManager() }
+    var selectedTab by remember { mutableStateOf(TabItem.LAUNCHES) }
 
     DisposableEffect(Unit) {
         onDispose {
@@ -77,10 +87,34 @@ fun App() {
                                 }
                             )
                         }
+                    },
+                    bottomBar = {
+                        NavigationBar {
+                            TabItem.values().forEach { tab ->
+                                NavigationBarItem(
+                                    icon = {
+                                        Icon(
+                                            imageVector = when (tab) {
+                                                TabItem.LAUNCHES -> Icons.Default.Add
+                                                TabItem.SETTINGS -> Icons.Default.Settings
+                                            },
+                                            contentDescription = tab.title
+                                        )
+                                    },
+                                    label = { Text(tab.title) },
+                                    selected = selectedTab == tab,
+                                    onClick = { selectedTab = tab }
+                                )
+                            }
+                        }
                     }
                 ) { paddingValues ->
                     Box(Modifier.padding(paddingValues)) {
-                        CurrentScreen()
+//                        CurrentScreen()
+                        when (selectedTab) {
+                            TabItem.LAUNCHES -> CurrentScreen()
+                            TabItem.SETTINGS -> SettingsScreen().Content()
+                        }
                     }
                 }
             }
