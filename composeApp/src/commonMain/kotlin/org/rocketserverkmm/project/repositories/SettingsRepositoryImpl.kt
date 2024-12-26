5a09912e52e0fd52a31da7ea525c7514ae9db870
egor.mikhalev <mamo4ka21196@gmail.com>
2024-12-26 15:47:10 +0300

    added methods to SettingsRepository

--- composeApp/src/commonMain/kotlin/org/rocketserverkmm/project/repositories/SettingsRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/org/rocketserverkmm/project/repositories/SettingsRepositoryImpl.kt
@@ -1,9 +1,22 @@
 package org.rocketserverkmm.project.repositories
 
+import kotlinx.coroutines.flow.MutableStateFlow
+import org.rocketserverkmm.project.domain.repositories.KeyVaultRepository
 import org.rocketserverkmm.project.domain.repositories.SettingsRepository
 
-class SettingsRepositoryImpl : SettingsRepository {
-    override suspend fun changeTheme(isDarkTheme: Boolean) {
-        TODO("Not yet implemented")
+class SettingsRepositoryImpl(
+    private val kVault: KeyVaultRepository
+) : SettingsRepository {
+    private val isDarkThemeFlow = MutableStateFlow(kVault.getBoolean(THEME_KEY) ?: false)
+
+    override suspend fun changeTheme(isDarkTheme: Boolean) : Boolean {
+        val isDark = !isDarkTheme
+        isDarkThemeFlow.value = isDarkTheme
+        kVault.saveBoolean(THEME_KEY, isDark)
+        return isDark
+    }
+
+    companion object {
+        private const val THEME_KEY = "THEME_IS_DARK"
     }
 }
