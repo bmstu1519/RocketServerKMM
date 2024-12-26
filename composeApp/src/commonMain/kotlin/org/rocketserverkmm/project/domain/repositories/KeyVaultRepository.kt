9c31be3ac37de229a583597c248d0fb09942ada2
egor.mikhalev <mamo4ka21196@gmail.com>
2024-12-26 15:46:50 +0300

    added methods to keyvault

--- composeApp/src/commonMain/kotlin/org/rocketserverkmm/project/domain/repositories/KeyVaultRepository.kt
+++ composeApp/src/commonMain/kotlin/org/rocketserverkmm/project/domain/repositories/KeyVaultRepository.kt
@@ -4,4 +4,6 @@ interface KeyVaultRepository {
     fun saveToken(key: String, value: String): Boolean
     fun getToken(forKey: String): String?
     fun deleteToken(forKey: String): Boolean
+    fun saveBoolean(forKey: String, value: Boolean): Boolean
+    fun getBoolean(forKey: String): Boolean?
 }
