215048483452dfa153cae2dcabbb3762db4f2754
egor.mikhalev <mamo4ka21196@gmail.com>
2025-01-30 16:44:19 +0300

    made two LocalTabNavigator for each tabs

--- composeApp/src/commonMain/kotlin/org/rocketserverkmm/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/rocketserverkmm/project/App.kt
@@ -3,16 +3,9 @@ package org.rocketserverkmm.project
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -21,33 +14,28 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import cafe.adriel.voyager.navigator.CurrentScreen
-import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.tab.TabNavigator
 import coil3.compose.setSingletonImageLoaderFactory
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.viewmodel.koinViewModel
 import org.rocketserverkmm.project.di.LocalScopeManager
 import org.rocketserverkmm.project.di.ScopeManager
 import org.rocketserverkmm.project.platform.RocketReserverKMMTheme
-import org.rocketserverkmm.project.presentation.screens.LaunchListScreen
-import org.rocketserverkmm.project.presentation.screens.SettingsScreen
 import org.rocketserverkmm.project.presentation.viewmodels.AppBootstrapViewModel
 import org.rocketserverkmm.project.settings.local.AsyncImageLoaderSingleton
 import org.rocketserverkmm.project.settings.tabSetting.TabItem
+import org.rocketserverkmm.project.settings.tabSetting.TabNavigationItem
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 @Preview
 fun App() {
     val scopeManager = remember { ScopeManager() }
-    var selectedTab by remember { mutableStateOf(TabItem.LAUNCHES) }
+
     koinViewModel<AppBootstrapViewModel>()
 
     DisposableEffect(Unit) {
@@ -62,7 +50,7 @@ fun App() {
                 AsyncImageLoaderSingleton.getAsyncImageLoader(context)
             }
 
-            Navigator(LaunchListScreen()) { navigator ->
+            TabNavigator(TabItem.LaunchesTab) { navigator ->
                 Scaffold(
                     topBar = {
                         Surface(
@@ -78,46 +66,28 @@ fun App() {
                                     actionIconContentColor = MaterialTheme.colorScheme.primary,
                                 ),
                                 navigationIcon = {
-                                    if (navigator.canPop) {
-                                        IconButton(onClick = { navigator.pop() }) {
-                                            Icon(
-                                                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                                                contentDescription = "Назад",
-                                                tint = MaterialTheme.colorScheme.onPrimary
-                                            )
-                                        }
-                                    }
+//                                    if (navigator.canPop) {
+//                                        IconButton(onClick = { navigator.current.}) {
+//                                            Icon(
+//                                                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+//                                                contentDescription = "Назад",
+//                                                tint = MaterialTheme.colorScheme.onPrimary
+//                                            )
+//                                        }
+//                                    }
                                 }
                             )
                         }
                     },
                     bottomBar = {
                         NavigationBar {
-                            TabItem.values().forEach { tab ->
-                                NavigationBarItem(
-                                    icon = {
-                                        Icon(
-                                            imageVector = when (tab) {
-                                                TabItem.LAUNCHES -> Icons.Default.Add
-                                                TabItem.SETTINGS -> Icons.Default.Settings
-                                            },
-                                            contentDescription = tab.title
-                                        )
-                                    },
-                                    label = { Text(tab.title) },
-                                    selected = selectedTab == tab,
-                                    onClick = { selectedTab = tab }
-                                )
-                            }
+                            TabNavigationItem(TabItem.LaunchesTab)
+                            TabNavigationItem(TabItem.SettingsTab)
                         }
                     }
                 ) { paddingValues ->
                     Box(Modifier.padding(paddingValues)) {
-//                        CurrentScreen()
-                        when (selectedTab) {
-                            TabItem.LAUNCHES -> CurrentScreen()
-                            TabItem.SETTINGS -> SettingsScreen().Content()
-                        }
+                        navigator.current.Content()
                     }
                 }
             }
--- composeApp/src/commonMain/kotlin/org/rocketserverkmm/project/settings/tabSetting/TabItem.kt
+++ composeApp/src/commonMain/kotlin/org/rocketserverkmm/project/settings/tabSetting/TabItem.kt
@@ -1,6 +1,68 @@
 package org.rocketserverkmm.project.settings.tabSetting
 
-enum class TabItem(val title: String) {
-    LAUNCHES("Запуски"),
-    SETTINGS("Настройки")
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
+import cafe.adriel.voyager.navigator.tab.Tab
+import cafe.adriel.voyager.navigator.tab.TabOptions
+import org.rocketserverkmm.project.presentation.screens.LaunchListScreen
+import org.rocketserverkmm.project.presentation.screens.SettingsScreen
+
+sealed class TabItem {
+    object LaunchesTab : Tab {
+        @Composable
+        override fun Content() {
+            Navigator(LaunchListScreen())
+        }
+
+        override val options: TabOptions
+            @Composable
+            get() = TabOptions(
+                index = 0u,
+                title = "Launches",
+                icon = rememberVectorPainter(Icons.Default.Add)
+            )
+    }
+
+    object SettingsTab : Tab {
+        @Composable
+        override fun Content() {
+            Navigator(SettingsScreen())
+        }
+
+        override val options: TabOptions
+            @Composable
+            get() = TabOptions(
+                index = 1u,
+                title = "Settings",
+                icon = rememberVectorPainter(Icons.Default.Settings)
+            )
+    }
+}
+
+@Composable
+fun RowScope.TabNavigationItem(tab: Tab) {
+    val tabNavigator = LocalTabNavigator.current
+
+    NavigationBarItem(
+        selected = tabNavigator.current == tab,
+        onClick = { tabNavigator.current = tab },
+        icon = {
+            Icon(
+                painter = tab.options.icon ?: rememberVectorPainter(
+                    Icons.Default.Add
+                ),
+                contentDescription = tab.options.title,
+            )
+        },
+        label = { Text(tab.options.title) }
+    )
 }
