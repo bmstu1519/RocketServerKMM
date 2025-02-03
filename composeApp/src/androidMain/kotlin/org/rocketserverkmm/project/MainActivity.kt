6cdf028f7a423b7ab1eeab38b651a37ebb8d2e7a
egor.mikhalev <mamo4ka21196@gmail.com>
2025-02-03 16:50:12 +0300

    moved key vault init method inside application class

--- composeApp/src/androidMain/kotlin/org/rocketserverkmm/project/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/rocketserverkmm/project/MainActivity.kt
@@ -6,13 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import org.rocketserverkmm.project.platform.initializeKVault
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        initializeKVault(this)
         setContent {
             App()
         }
